f90c3b660468f93b1eaf9e3c03d7511b822bf8bb
wangjojo <weiainijiujiu@126.com>
2020-11-16 14:56:44 +0800

    演示页面,demo2完成

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,4 +1,5 @@
-let p;
+let p
+let child
 const ref = Vue.ref
 let app = Vue.createApp({
   setup() {
@@ -16,17 +17,39 @@ let app = Vue.createApp({
     const list = [{ name: 'foo', age: 11}, { name: 'foo2', age: 12}]
     const obj1 = { name: 'foo', age: 11}
     let data = ref([obj1, list])
+    let opener =ref(null)
+    const openNewWin = (page) => {
+      opener.value = window.open(page,"_blank","toolbar=yes, location=yes, directories=no, status=no, menubar=yes, scrollbars=yes, resizable=no, copyhistory=yes, width=400, height=400");
+      child = new PostMessager({
+        id: 'child2',
+        target: opener.value,
+        targetId: 'child1'
+      });
+    }
     return {
       data,
+      opener,
       messageFromOrther,
+      openNewWin
     }
 
   },
   data() {
     return {
       form: {
+        newWinMessage: '',
         message: '',
         data: '0'
+      },
+      rules: {
+        newWinMessage: [
+          { validator: (rule, value, callback) => {
+            if (!child) {
+              return Promise.reject('请先点击「打开新窗口」按钮');
+            }
+            return Promise.resolve();
+          }, trigger: 'change' }
+        ]
       }
     }
   },
@@ -34,6 +57,11 @@ let app = Vue.createApp({
     send() {
       const data = this.data
       p.send('child', { message:  this.form.message, data:  getPlainObject(data[this.form.data]) })
+    },
+    sendNewWinMessage() {
+      if (child){
+        child.targetEmit('some-event', { text: this.form.newWinMessage })
+      }
     }
   }
 })
